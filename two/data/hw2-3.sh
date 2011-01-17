1e83a7bb0b2646bd2d21eb42929a1c91958001aa
Brian Gianforcaro <b.gianfo@gmail.com>
2011-01-17 16:00:22 -0500

    Add tests for different schedules

--- two/data/hw2-3.sh
+++ two/data/hw2-3.sh
@@ -7,7 +7,18 @@ MITR=8191
 N=20000000
 
 $JAVA MSAreaSeq $SEED $MITR $N
-$JAVA -Dpj.nt=1 MSAreaSmp $SEED $MITR $N
-$JAVA -Dpj.nt=2 MSAreaSmp $SEED $MITR $N
-$JAVA -Dpj.nt=4 MSAreaSmp $SEED $MITR $N
-$JAVA -Dpj.nt=8 MSAreaSmp $SEED $MITR $N
+echo "fixed"
+$JAVA -Dpj.schedule=fixed -Dpj.nt=1 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=fixed -Dpj.nt=2 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=fixed -Dpj.nt=4 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=fixed -Dpj.nt=8 MSAreaSmp $SEED $MITR $N
+echo "dynamic"
+$JAVA -Dpj.schedule=dynamic -Dpj.nt=1 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=dynamic -Dpj.nt=2 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=dynamic -Dpj.nt=4 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=dynamic -Dpj.nt=8 MSAreaSmp $SEED $MITR $N
+echo "guided"
+$JAVA -Dpj.schedule=guided -Dpj.nt=1 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=guided -Dpj.nt=2 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=guided -Dpj.nt=4 MSAreaSmp $SEED $MITR $N
+$JAVA -Dpj.schedule=guided -Dpj.nt=8 MSAreaSmp $SEED $MITR $N
